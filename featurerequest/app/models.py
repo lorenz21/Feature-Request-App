788039b460ab55321878a85cce4679826bedeea2
Steve <lorenz21@hawaii.edu>
2018-05-19 14:10:10 -1000

    custom error handling

--- featurerequest/app/models.py
+++ featurerequest/app/models.py
@@ -9,6 +9,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     requests = db.relationship('Request', backref='requestor', lazy='dynamic')
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
--- featurerequest/app/routes.py
+++ featurerequest/app/routes.py
@@ -4,6 +4,7 @@ from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
+from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
@@ -46,3 +47,19 @@ def register():
         flash('Welcome, you are now a registered user.')
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    features = [
+        {'requestor': user, 'description': 'New heading'},
+        {'requestor': user, 'description': 'More home page space'}
+    ]
+    return render_template('user.html', user=user, features=features)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
